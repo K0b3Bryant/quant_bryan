1ead0936dcf9bafa62d8263e1996530f0d1f4ac4
Marco <sejereje1011@gmail.com>
2025-06-16 10:52:00 +0200

    Create config.py

---
+++ factors/config.py
@@ -0,0 +1,7 @@
+CONFIG = {
+    'date_format': '%Y-%m-%d',
+    'p_value_threshold': 0.05,
+    'return_type': 'log',  # Options: 'log' or 'simple'
+    'lasso_alpha': 0.01,
+    'ridge_alpha': 0.01
+}
