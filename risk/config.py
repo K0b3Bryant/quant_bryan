8778ec48be8bef7373139b3dda394ca006f470b3
Marco <sejereje1011@gmail.com>
2025-06-16 11:08:05 +0200

    Create config.py

---
+++ risk/config.py
@@ -0,0 +1,6 @@
+CONFIG = {
+    'date_format': '%Y-%m-%d',  # Format of the date column
+    'assets': ['Asset1', 'Asset2'],  # List of assets in the portfolio
+    'lag': 1,  # Lag for positions (default is 1 day)
+    'confidence_level': 0.95  # Confidence level for risk measures
+}
