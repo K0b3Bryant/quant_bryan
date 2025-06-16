80bc7c773f0158038228010d5ed6ea8ead601981
Marco <sejereje1011@gmail.com>
2025-06-16 11:19:12 +0200

    Create config.py

---
+++ backtesting/config.py
@@ -0,0 +1,6 @@
+CONFIG = {
+    'date_format': '%Y-%m-%d',  # Format of the date column
+    'lag': 1,  # Lag for positions (default is 1 day)
+    'transaction_cost': 0.001,  # Transaction costs as a percentage
+    'rolling_window': 20  # Rolling window size for metrics
+}
